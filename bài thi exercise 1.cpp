c1469cfd45c1079568f9c1cee5f8ab34836338ac
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2022-08-24 15:24:56 +0700

    nộp bài thi exercise 1

--- bài thi exercise 1.cpp
+++ bài thi exercise 1.cpp
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(){
-int num,i,sum=0;
-printf("nhập số nguyên từ bàn phím: ");
-scanf("%d",&num);
-printf("1+");
-for(i=2 ; i<=num-1 ; i++)
-	printf("1/%d+",i);
-    for(i=1 ; i<=num ; i++)
-    	sum=sum+i;
-        printf("1/%d",num);
-        printf("\nS=1/%d",sum+1/num);
+    int num,i,sum=0;
+    printf("nhập số nguyên từ bàn phím: ");
+    scanf("%d",&num);
+    printf("1+");
+    for(i=2 ; i<=num-1 ; i++)
+        printf("1/%d+",i);
+        for(i=1 ; i<=num ; i++)
+    	    sum=sum+i;
+            printf("1/%d",num);
+            printf("\nS=1/%d",sum+1/num);
+        
 }
 
 
